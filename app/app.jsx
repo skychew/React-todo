2135ab23f0ae753d2e956297c758c0244fe0589e
Sky Chew <sky@skychew.com>
2017-01-31 13:35:52 +0800

    add TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp'); // check webpack config because its been told to find in app components
+
 // Load foundation
 $(document).foundation();
 
 // App css
 require('style!css!sass!applicationStyles')
 
-ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+ReactDOM.render
+
+  <TodoApp/>,
   document.getElementById('app')
 );
