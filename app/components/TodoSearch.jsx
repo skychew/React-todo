7ad21801454c2f6aef2f97377445edde96d81b28
Sky Chew <sky@skychew.com>
2017-02-01 18:45:00 +0800

    test search component

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -18,6 +18,7 @@ var TodoSearch = React.createClass({
         <div>
           <label>
             <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+          Show Completed Tasks
           </label>
         </div>
       </div>
