f25ed94a3b3a64d53ad1eb04dc6c500266b70e40
Sky Chew <sky@skychew.com>
2017-02-10 08:06:41 +0800

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,12 +17,33 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
-    /* similar to the single line return above
-    if($.isArray(todos)){
-      return todos;
-    }else{
-      return [];
-    }
-    */
+
+  },
+  filterTodos: function (todos,showCompleted,searchText){
+    var filteredTodos =todos;
+
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo)=>{
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo)=>{
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //Sort todos with non-completed first
+    filteredTodos.sort((a,b) =>{
+      if(!a.completed && b.completed){
+        return -1; //
+      }else if(a.completed && !b.completed) {
+        return 1; //a should come after b
+      }else{
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -49,4 +49,46 @@ describe('TodoAPI',()=>{
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos',()=>{
+    var todos = [{
+      id:1,
+      text:'some text here',
+      completed:true
+    },{
+      id:2,
+      text:'other text here2',
+      completed:false
+    },{
+      id:3,
+      text:'some text here3',
+      completed:true
+
+    }];
+
+    it('should return allitems if showComplete is true', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos when showComplete is false', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,false,'');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
