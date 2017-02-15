9fe81d1c8ab7d35238d86ad2966f4479e46e3728
Sky Chew <sky@skychew.com>
2017-02-15 19:23:20 +0800

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,16 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp'); // check webpack config because its been told to find in app components
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(()=>{
+  console.log('New state',store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 // Load foundation
 $(document).foundation();
 
